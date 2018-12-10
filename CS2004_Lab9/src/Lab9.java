905b99a54f9ccec652f12382306af67ed0bbeab3
Brandon <1624079@brunel.ac.uk>
2018-12-10 14:41:10 +0000

    initial commit

---
+++ CS2004_Lab9/src/Lab9.java
@@ -0,0 +1,11 @@
+public class Lab9 {
+	public static void main(String[]args) {
+		ScalesSolution s = new ScalesSolution("1111111");
+		s.println();
+		s.SmallChange();
+		s.println();
+		
+		
+
+	}
+}
